333a267d8d8ee9f3981dc65e62fc29c53afd5c1b
Riderfan1910 <45297521+Riderfan1910@users.noreply.github.com>
2019-10-03 16:57:15 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+function launchFullScreen(element) {
+  if(element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  }
+}
+
+// Launch fullscreen for browsers that support it!
+launchFullScreen(document.documentElement); // the whole page
+launchFullScreen(document.getElementById("videoElement")); /
